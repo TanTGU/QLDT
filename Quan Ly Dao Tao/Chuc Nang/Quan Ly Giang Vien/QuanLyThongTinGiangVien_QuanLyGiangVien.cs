cf74c5456ea09f12add74b2e24d7084d70435cb3
HoangHaKai <nhokno2003@gmail.com>
2023-11-10 13:35:02 +0700

    aa

--- Quan Ly Dao Tao/Chuc Nang/Quan Ly Giang Vien/QuanLyThongTinGiangVien_QuanLyGiangVien.cs
+++ Quan Ly Dao Tao/Chuc Nang/Quan Ly Giang Vien/QuanLyThongTinGiangVien_QuanLyGiangVien.cs
@@ -90,7 +90,7 @@ namespace Quan_Ly_Dao_Tao.Chuc_Nang.Quan_Ly_Giang_Vien
 
         private void listDS_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            //string ma = listDS.SelectedItems[0].SubItems[0];
         }
 
         private void cbdonvi_SelectedIndexChanged(object sender, EventArgs e)
